c7a7697296344429444312c53c686684628eda63
MilenaSilveira <120193252+MilenaSilveira@users.noreply.github.com>
2023-01-29 12:39:22 -0500

    added id func and routes

--- server.js
+++ server.js
@@ -5,10 +5,36 @@ const fs = require('fs');
 //Helper method.
 
 const PORT = 3001;
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
+//Route to home page.
+app.get('/', (req, res) =>
+res.sendFile(path.join(Develop, './public/index.html')));
+
+//Route to notes page.
+app.get('/notes', (req, res) => 
+res.sendFile(path.join(Develop, './public/notes.html')));
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Localhost
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);

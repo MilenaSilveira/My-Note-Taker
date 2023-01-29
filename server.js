0afbbe106ed69d71e323bfaa4c07d0168946d519
MilenaSilveira <120193252+MilenaSilveira@users.noreply.github.com>
2023-01-29 13:09:50 -0500

    Added helpers and get post func.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 //Helper method.
+const uuid = require('./helpers/uuid');
 
 const PORT = 3001;
 const app = express();
@@ -11,13 +12,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
-//Route to home page.
+//Route to homepage.
 app.get('/', (req, res) =>
-res.sendFile(path.join(Develop, './public/index.html')));
+res.sendFile(path.join(__dirname, './public/index.html')));
 
 //Route to notes page.
 app.get('/notes', (req, res) => 
-res.sendFile(path.join(Develop, './public/notes.html')));
+res.sendFile(path.join(__dirname, './public/notes.html')));
+
+//Get Notes
+app.get('/api/notes', (req, res) => {
+res.status(200).json(`${req.method} request received to get note.`)
+    console.info(`${req.method} request received to get note.`)
+});
+
+//POST request to add note
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note.`);
+ 
+})
+
+//Log note on terminal
+
+//Post note to add new review
 
 //
 

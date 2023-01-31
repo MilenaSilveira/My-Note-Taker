749dbb129b95f65365d4a86cdf74da5a00fb2dbf
MilenaSilveira <120193252+MilenaSilveira@users.noreply.github.com>
2023-01-31 16:00:05 -0500

    changes to homeroute

--- routes/homeroutes.js
+++ routes/homeroutes.js
@@ -1,11 +1,13 @@
 const router = require ('express').Router();
 const path = require('path');
-//Route to homepage.
-router.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, '../public/index.html')));
+
 
 //Route to notes page.
 router.get('/notes', (req, res) => 
 res.sendFile(path.join(__dirname, '../public/notes.html')));
+
+//Route to homepage.
+router.get('*', (req, res) =>
+res.sendFile(path.join(__dirname, '../public/index.html')));
 
 module.exports = router
--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
-
-app.use(homeRoutes)
 app.use(apiRoutes)
+app.use(homeRoutes)
+
 
 
 //Localhost

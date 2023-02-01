ee2987080b3f4ffe1af3809f03ec85ce4a6cbe99
MilenaSilveira <120193252+MilenaSilveira@users.noreply.github.com>
2023-02-01 09:35:41 -0500

    port1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const homeRoutes = require('./routes/homeroutes');
 const apiRoutes = require('./routes/apiroutes')
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
